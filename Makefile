aa3b68fc281aba7f6a3ca476716fa4c4b429a28a
asmundberge <herrasbe@gmail.com>
2017-01-15 17:06:20 +0100

    Embed styles

--- Makefile
+++ Makefile
@@ -1,12 +1,9 @@
 all: elm
 
-elm: app.js styles.css
+elm: app.js 
 
 app.js: frontend/*.elm
 	elm-make frontend/Main.elm --yes --output static/app.js
-
-styles.css: frontend/Styles/*.elm
-	elm-css frontend/Styles/stylesheet.elm --output static/
 
 clean:
 	-rm -r static/app.js static/styles.css node_modules/ elm-stuff/ 
--- frontend/View.elm
+++ frontend/View.elm
@@ -3,12 +3,15 @@ module View exposing (..)
 import Html exposing (Html, div, h1, p, ul, li, text)
 import Model exposing (Model, Cat)
 import Message exposing (Msg)
+import Css
+import Styles.Styles as Styles
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ h1 [] [ text "servant-elm-poc" ]
+        [ node "style" (Css.compile [ Styles.css ])
+        , h1 [] [ text "servant-elm-poc" ]
         , p [] [ text "The API returned the following cats:" ]
         , ul []
             (List.map
