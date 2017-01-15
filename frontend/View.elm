b6cf060fcb5e0ab64955d017f8ed7a3b7b37c6ed
asmundberge <herrasbe@gmail.com>
2017-01-15 17:16:28 +0100

    Fix embedded styles

--- frontend/View.elm
+++ frontend/View.elm
@@ -1,6 +1,6 @@
 module View exposing (..)
 
-import Html exposing (Html, div, h1, p, ul, li, text)
+import Html exposing (Html, node, div, h1, p, ul, li, text)
 import Model exposing (Model, Cat)
 import Message exposing (Msg)
 import Css
@@ -10,7 +10,7 @@ import Styles.Styles as Styles
 view : Model -> Html Msg
 view model =
     div []
-        [ node "style" (Css.compile [ Styles.css ])
+        [ node "style" [] [ Css.compile [ Styles.css ] |> .css |> text ]
         , h1 [] [ text "servant-elm-poc" ]
         , p [] [ text "The API returned the following cats:" ]
         , ul []
